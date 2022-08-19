b94d051714b9ca11a43eb5835ea7b61e96831dc8
Mari-glo <marigloria@gmail.com>
2022-08-19 18:47:02 -0400

    actualizacion

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Buenasnoticias
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.path << rails.root.join ("app", "assets", "images")
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +19,6 @@ module Buenasnoticias
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
   end
 end
